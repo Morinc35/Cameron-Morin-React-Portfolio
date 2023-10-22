74fd8d6fc17f63d512b75073cbe3901ec898660f
Cameron Morin <morinc35@gmail.com>
2023-10-22 12:28:44 -0400

    fixing footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,17 @@
 export default function Footer() {
 return(
-    <footer style={{backgroundColor:'#6b6a6a', margin: ''}}>
-    <div className="footer-image" style={{display: "flex", justifyContent: 'center'}}>
-    <a className="image is-48x48 mx-3" href='https://github.com/Morinc35'><img src="src\assets\icons8-github-96.png"  alt=""></img></a>
-    <a className="image is-48x48 mx-3" href='https://www.linkedin.com/in/cameron-morin-58aba01a5/'><img src="src\assets\icons8-linkedin-96.png" alt=""></img></a>
-    <a className="image is-48x48 mx-3"><img src="src\assets\icons8-gmail-50.png" alt=""></img></a>
+    <footer style={{ backgroundColor: '#6b6a6a',  position: 'absolute', bottom: '0', left: '0', right: '0', display: 'flex', flexDirection: 'column', alignItems: 'center', fontFamily: 'monospace', textAlign: 'center' }}>
+    <div className="footer-image is-flex">
+      <a className="image is-48x48 mx-3" href='https://github.com/Morinc35'><img src="src/assets/icons8-github-96.png" alt="GitHub" /></a>
+      <a className="image is-48x48 mx-3" href='https://www.linkedin.com/in/cameron-morin-58aba01a5/'><img src="src/assets/icons8-linkedin-96.png" alt="LinkedIn" /></a>
+      <a className="image is-48x48 mx-3"><img src="src/assets/icons8-gmail-50.png" alt="Gmail" /></a>
     </div>
-    <ul style={{
-  display: 'flex',
-  justifyContent: 'center',
-  fontFamily: 'monospace'
-}}>
-  <li>
-    <h1 className="footer-react has-text-black">Made using React</h1>
-  </li>
-  <li className='footer-icons has-text-black ml-5'>Icons from <a className="icon-link has-text-black" href="https://icons8.com/">icons8</a></li>
-</ul>
-
-    </footer>
+    <ul style={{ display: 'flex', justifyContent: 'center', fontFamily: 'monospace' }}>
+      <li>
+        <h1 className="footer-react has-text-black">Made using React</h1>
+        <h1 className='footer-icons has-text-black'>Icons from <a className="icon-link has-text-black is-underlined" href="https://icons8.com/">icons8</a></h1>
+      </li>
+    </ul>
+  </footer>
 )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,10 +9,11 @@ return(
       textAlign: 'center',
     }}
   >
-    <div className="image mt-1 is-2by">
+    <div className="image mt-2 is-2by">
       <img className="is-rounded" src="src/assets/cropped-Morin.png" alt="Picture of Cameron Morin" />
     </div>
-    <h1 className="is-size-1">Hi! I am Cameron Morin.</h1>
+    <h1 className="is-size-1 has-text-black">Hi! I am Cameron Morin.</h1>
+    <h2 className="is-size-2 has-text-black">I am a passionte web developer with experience in HTML, CSS, and Javascript. I have experience using SQL, and Mongodb as backend frameworks. Lets connect!</h2>
   </section>
 )
 }
