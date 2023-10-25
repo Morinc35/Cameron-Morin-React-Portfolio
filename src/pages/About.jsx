a2732ff57ae7c59a8765c4fa37061c711ce6606a
Cameron <cameron@admins-mbp.mynetworksettings.com>
2023-10-25 17:51:35 -0400

    photo fixes v2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,7 +10,7 @@ return(
     }}
   >
     <div className="image mt-2 is-2by">
-      <img className="is-rounded" src="src/assets/cropped-Morin.png" alt="Picture of Cameron Morin" />
+      <img className="is-rounded" src="/cropped-Morin.png" alt="Picture of Cameron Morin" />
     </div>
     <h1 className="is-size-1 has-text-black">Hi! I am Cameron Morin.</h1>
     <h2 className="is-size-2 has-text-black">I am a passionte web developer with experience in HTML, CSS, and Javascript. I have experience using SQL, and Mongodb as backend frameworks. Lets connect!</h2>
